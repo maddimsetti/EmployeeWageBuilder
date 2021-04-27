67bf9e9b434e6049a5b8d68af82b8d84d47cceb4
prudhvi <prudhvieethakota123@gmail.com>
2021-04-27 22:28:40 +0530

    Added:Calculated the Daily Employee Wage using Switch case statement

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,25 +6,26 @@ public class EmployeeWageBuilder {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the Employee Wage Computation Program");
 
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
 		int Wage_Per_Hour = 20;
 		int empWorkingHours = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		   if ( empCheck == IS_FULL_TIME ) {
-		      System.out.println("Employee is present");
-		      empWorkingHours = 8;
+		int empCheck = (int) Math.floor(Math.random() * 100) % 3;
+		  switch (empCheck) {
+		  case IS_FULL_TIME:
+		        System.out.println("Employee is present");
+		        empWorkingHours = 8;
+		        break;
+	          case IS_PART_TIME:
+			System.out.println("Employee is Present");
+			empWorkingHours = 4;
+		        break;
+		  default:
+			System.out.println("Employee is Absent");
+			break;
 		   }
-		   else if ( empCheck == IS_PART_TIME ) {
-				System.out.println("Employee is Present");
-				empWorkingHours = 4;
-		   }
-		   else {
-			  System.out.println("Employee is Absent");
-			  empWorkingHours = 0;
-		   }
-		int dailyEmployeeWage = Wage_Per_Hour * empWorkingHours;
-       	        System.out.println("dailyEmployeeWage:"+dailyEmployeeWage);
+              int dailyEmployeeWage = Wage_Per_Hour * empWorkingHours;
+              System.out.println("dailyEmployeeWage:"+dailyEmployeeWage);
 	}
 }
