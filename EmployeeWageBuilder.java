b145dd283da4e86a881ff4167fe68e2625e1b7e1
prudhvi <prudhvieethakota123@gmail.com>
2021-04-30 00:03:11 +0530

    Added:Refactor the code and Calculated Wages Till a Condition of Total Working Hours of 100 or Maximum days of 20 is reached for a month

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,43 +2,47 @@ package employeewage;
 
 public class EmployeeWageBuilder {
 
+	  public static final int IS_FULL_TIME = 1;
+	  public static final int IS_PART_TIME = 2;
+	  public static final int WAGE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the Employee Wage Computation Program");
 
-	  final int IS_FULL_TIME = 1;
-	  final int IS_PART_TIME = 2;
-	  int Wage_Per_Hour = 20;
-	  int workingDaysPermonth = 20;
-	  int empHours_inMonth = 100;
+	        int workingDaysPermonth = 20;
+	        int empHours_inMonth = 100;
 
-	  int empWorkingHours = 0;
-	  int totalEmployeeWage = 0;
-	  System.out.printf("%5s     %5s     %5s     %5s\n","Day", "empWorkingHours", "dailyWage", "totalEmpWorkingHours");
+	        int empWorkingHours = 0;
+	        int totalEmpWorkingHours =0;
+	        int totalWorkingDays = 0;
 
-	  for(int day = 1, totalEmpWorkingHours = 0; day <= workingDaysPermonth && totalEmpWorkingHours < empHours_inMonth;
-				day++, totalEmpWorkingHours += empWorkingHours)
-	  {
+	        while (totalEmpWorkingHours <= empHours_inMonth && totalWorkingDays < workingDaysPermonth )
+	        {
+
+		totalWorkingDays++;
 		int empCheck = (int) Math.floor(Math.random() * 100) % 3;
 
-		  switch (empCheck) {
-		  case IS_FULL_TIME:
-		        System.out.println("Employee is present");
-		        empWorkingHours = 8;
-		        break;
-	          case IS_PART_TIME:
-			System.out.println("Employee is Present");
-			empWorkingHours = 4;
-		        break;
-		  default:
-			System.out.println("Employee is Absent");
-			break;
+		   switch (empCheck) {
+		      case IS_FULL_TIME:
+		           System.out.println("Employee is present");
+		           empWorkingHours = 8;
+		           break;
+	              case IS_PART_TIME:
+		       	   System.out.println("Employee is Present");
+			   empWorkingHours = 4;
+		           break;
+		      default:
+		      	   System.out.println("Employee is Absent");
+			   break;
 		   }
-              int dailyEmployeeWage = Wage_Per_Hour * empWorkingHours;
-              totalEmployeeWage += dailyEmployeeWage;
-              System.out.printf("%5d     %5d     %5d     %5d\n",day, empWorkingHours, dailyEmployeeWage, totalEmpWorkingHours + empWorkingHours);           
 
-	  }
-	  System.out.println("Total Salary for a Month:" + totalEmployeeWage);
+		   totalEmpWorkingHours += empWorkingHours;
+		   System.out.println("Day# " +totalWorkingDays + "EmpWorkingHours:" +empWorkingHours);
+	       }
+
+               int totalEmployeeWage = totalEmpWorkingHours * WAGE_PER_HOUR;
+	       System.out.println("Total Salary for a Month:" + totalEmployeeWage);
 	}
+
 }
